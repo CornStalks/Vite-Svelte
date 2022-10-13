9278b323d0a3ff5b872344775ea92a27e25fb7e2
CornStalks <114187354+CornStalks@users.noreply.github.com>
2022-10-13 15:22:11 -0700

    1 file modified

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,35 @@
-{
+<script>
+
+let children_in_basement = 0
+
+function add_a_child(){
+children_in_basement +=1
+}
+
+</script>
+
+
 <h1> 
 Hello. This is a level one heading
 </h1>
-background: red
-}
+
 <p>This is a paragraph</p>
 
 <h2> This is a level two heading </h2>
 
+<style> 
+h1 {
+
+background: red
+
+}
+</style>
+
+<p> Put a child in your basement </p>
+
+{
+children_in_basement
+
+}
+<button>
+Add one child to your basement</button>
