8652f1545b2544a197ed11f704491d5a97933c15
CornStalks <114187354+CornStalks@users.noreply.github.com>
2022-11-01 15:17:25 -0700

    1 file modified

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,29 @@
 <script>
 
+let randomnumber = 0
+
 let children_in_basement = 0
 
 function add_a_child(){
 children_in_basement +=1
 }
+function kill_a_child(){
+children_in_basement -=1
+}
 
+let YourName = "" // Btw doulbe quotes ("") is a string!
 </script>
 
+<input type="text" bind:value={YourName} placeholder="Your Name"/>
+<input type="number" bind:value={children_in_basement} min="0" max="100" />
+<input type="range" bind:value={randomnumber} min="-100" max="100" />
+<input type="number" bind:value={randomnumber}/> 
+ 
+<!-- Idk why this wierd comment symbols are here. But anyways...
+There are many different types of inputs. there are checkboxes as well as other stuff!-->
 
 <h1> 
-Hello. This is a level one heading
+Hello {YourName}. This is a level one heading
 </h1>
 
 <p>This is a paragraph</p>
@@ -25,11 +38,14 @@ background: red
 }
 </style>
 
-<p> Put a child in your basement </p>
+<p> Put a child in your basement: </p>
+<p>Number of Children: {children_in_basement} </p>
 
-{
-children_in_basement
 
-}
-<button>
+<button on:click={add_a_child}>
+
 Add one child to your basement</button>
+
+<button on:click={kill_a_child}>
+
+Free a child (why would u?) </button>
