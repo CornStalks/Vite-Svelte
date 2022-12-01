76845fb5f6241ff4f35078c374a89146a5609d4c
CornStalks <114187354+CornStalks@users.noreply.github.com>
2022-12-01 12:55:10 -0800

    1 file modified

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script>
 
+let test1 = 0
+
 let randomnumber = 0
 
 let children_in_basement = 0
@@ -11,6 +13,10 @@ function kill_a_child(){
 children_in_basement -=1
 }
 
+function test(){
+  test1 +=1
+}
+
 let YourName = "" // Btw doulbe quotes ("") is a string!
 </script>
 
@@ -19,7 +25,7 @@ let YourName = "" // Btw doulbe quotes ("") is a string!
 <input type="range" bind:value={randomnumber} min="-100" max="100" />
 <input type="number" bind:value={randomnumber}/> 
  
-<!-- Idk why this wierd comment symbols are here. But anyways...
+<!-- Idk why this wierd comment symbols are here, that's how html does it ig smh... But anyways...
 There are many different types of inputs. there are checkboxes as well as other stuff!-->
 
 <h1> 
@@ -49,3 +55,5 @@ Add one child to your basement</button>
 <button on:click={kill_a_child}>
 
 Free a child (why would u?) </button>
+
+<button on:click={test}
